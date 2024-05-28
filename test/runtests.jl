f5b9c847d23a770d84deb26e10cab41fbc4b7c6e
Akihiro Nakamura <nakamura27@bpe.es.osaka-u.ac.jp>
2024-05-28 13:13:15 +0900

    update LIFNeuron test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,20 @@ using Test
 
 @testset "SNNLab.jl" begin
     # Write your tests here.
-    true
+    
+end
+
+@testset "LIFNeuron Tests" begin
+    # LIFNeuronモジュール内の関数や構造体のテスト
+    # update! メソッドが実装されているかのテスト
+    @test isdefined(LIFNeuron, :update!)
+    # get_spike メソッドが実装されているかのテスト
+    @test isdefined(LIFNeuron, :get_spike)
+    # get_v メソッドが実装されているかのテスト
+    @test isdefined(LIFNeuron, :get_v)
+
+    # 例えばLIFNeuronモジュールに LIFNeuron 構造体があり、コンストラクタに引数を渡す場合
+    N=2
+    neurons = LIF{Float32}(N=N)
+    @test neurons.N == N
 end
