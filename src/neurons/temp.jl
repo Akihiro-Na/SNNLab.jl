8a41cf7fa79be8ae008d743365a7c7c4c6f1a649
Akihiro Nakamura <nakamura27@bpe.es.osaka-u.ac.jp>
2024-05-28 12:41:10 +0900

    Temporarily rename Neuron.jl to temp.jl

---
+++ src/neurons/temp.jl
@@ -0,0 +1,22 @@
+# src/neurons/Neuron.jl
+module Neuron
+
+# 抽象型の定義
+abstract type AbstractNeuron{FT} end
+
+abstract type AbstractNeuronParam{FT} end
+
+# update! メソッドの抽象定義
+function update!(neuron::AbstractNeuron{FT}, param::AbstractNeuronParam{FT}, dt::FT, Ie::Vector{FT}) where FT
+	throw(MethodError(update!, (neuron, param, dt, Ie)))
+end
+
+function get_spike(neuron::AbstractNeuron)::Vector{Bool}
+    throw(MethodError(get_spike,()))
+end
+
+function get_v(neuron::AbstractNeuron{FT})::Vector{FT} where FT
+    throw(MethodError(get_v,()))
+end
+
+end # module Neuron
