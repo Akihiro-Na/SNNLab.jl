2f53d556d5b06eababa2972c36a7d52adb71fec2
Akihiro Nakamura <nakamura27@bpe.es.osaka-u.ac.jp>
2024-06-05 20:06:16 +0900

    add def of state2lambda

---
+++ src/env_agent_interface/AbEnvAgentInterface.jl
@@ -0,0 +1,6 @@
+# src/env_agent_interface/AbEnvAgentInterface.jl
+
+# 抽象型の定義
+abstract type AbstractEnvAgentInrerface{FT} end
+
+abstract type AbstractEnvAgentInrerfaceParam{FT} end
---
+++ src/env_agent_interface/state2lambda.jl
@@ -0,0 +1,48 @@
+
+# state2λ のパラメータ(固定)
+@kwdef struct state2λParameter{FT} <: AbstractEnvAgentInrerfaceParam{FT}
+    λmax::FT = 400 # 最大の発火頻度 [Hz]
+    σ::FT = 2 # place cell の配置間隔 [m]
+    xmin::FT = -2
+    xmax::FT = 22
+    ymin::FT = -2
+    ymax::FT = 22
+    receptive_centers::Vector{Tuple{FT,FT}} = vec(collect(Iterators.product(xmin:σ:xmax, ymin:σ:ymin))) # 
+    N::UInt32 = length(receptive_centers)
+end
+
+# state2λ modelの定義
+@kwdef mutable struct state2λ{FT} <: AbstractEnvAgentInrerface{FT}
+    param::state2λParameter = state2λParameter{FT}()
+    λvec::Vector{FT} = zeros(FT, param.N)
+    normvec::Vector{FT} = zeros(FT, param.N)  # norm保存用vector
+end
+
+function vector_norm_grid!(norm_grid::Vector{FT}, grid::Vector{Tuple{FT,FT}}, x, y)::Vector{FT} where FT
+    #=
+    for (i, (a, b)) in enumerate(grid)
+        norm_grid[i] = norm([a - x, b - y])
+    end
+    =#
+    @inbounds for i in eachindex(grid)
+        a, b = grid[i]
+        norm_grid[i] = sqrt((a - x)^2 + (b - y)^2)
+    end
+    return norm_grid
+end
+
+# The case of 2D state 
+function update!(lambda::state2λ{FT}, param::state2λParameter{FT}, state::Tuple{FT, FT})::Vector{FT} where FT
+    x, y = state
+    @unpack λmax, σ, receptive_centers = param
+    vector_norm_grid!(lambda.normvec, receptive_centers, x, y)
+    for i in eachindex(lambda.normvec)
+        lambda.λvec[i] = λmax * exp(-lambda.normvec[i]^2 / σ^2)
+    end
+    return lambda.λvec
+end
+
+#=
+old 
+  0.272270 seconds (1.95 M allocations: 142.333 MiB, 53.03% gc time, 13.14% compilation time)
+=#
