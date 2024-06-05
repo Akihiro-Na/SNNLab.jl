76945209b584fda9c0162cf98b6e483493cbe760
Akihiro Nakamura <nakamura27@bpe.es.osaka-u.ac.jp>
2024-06-05 20:05:56 +0900

    add env_agent_interface

--- src/SNNLab.jl
+++ src/SNNLab.jl
@@ -26,13 +26,22 @@ include("environments/AbEnvironment.jl")
 include("environments/Maze.jl")
 # end of synapse model ==========
 
+# env_agent_interface =================
+include("env_agent_interface/AbEnvAgentInterface.jl")
+include("env_agent_interface/state2lambda.jl")
+# end of env_agent_interface ==========
+
 # Neuron model
 export LIF, PPPNeuron
 # Synapse model
 export DExpSynapse
 # Environment model
 export Maze
+# env_agent_interface
+export state2λ
+
 # Multiple dispatch function
 export update!,init!
+
 
 end # module
