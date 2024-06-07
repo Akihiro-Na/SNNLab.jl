7507e1efbbee7c71da50baf957c6c02c7372c1ba
Akihiro Nakamura <nakamura27@bpe.es.osaka-u.ac.jp>
2024-06-07 09:47:03 +0900

    add UIT

--- src/env_agent_interface/AbEnvAgentInterface.jl
+++ src/env_agent_interface/AbEnvAgentInterface.jl
@@ -1,6 +1,8 @@
 # src/env_agent_interface/AbEnvAgentInterface.jl
 
 # 抽象型の定義
-abstract type AbstractEnvAgentInrerface{FT} end
+#abstract type AbstractEnvAgentInrerface{FT} end
+abstract type AbstractEnvAgentInrerface{FT,UIT} end
 
-abstract type AbstractEnvAgentInrerfaceParam{FT} end
+#abstract type AbstractEnvAgentInrerfaceParam{FT} end
+abstract type AbstractEnvAgentInrerfaceParam{FT,UIT} end
