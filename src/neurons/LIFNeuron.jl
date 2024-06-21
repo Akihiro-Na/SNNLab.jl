59a7f6e0b16746e6f1307f87bfd40fd7603def9b
Akihiro Nakamura <nakamura27@bpe.es.osaka-u.ac.jp>
2024-06-21 20:24:57 +0900

    add update! with vector

--- src/neurons/LIFNeuron.jl
+++ src/neurons/LIFNeuron.jl
@@ -43,6 +43,26 @@ function update!(neurons::LIF{FT,UIT}, param::LIFParameter{FT}, dt::FT, Ie::SubA
     neurons.tcount += 1
 end
 
+function update!(neurons::LIF{FT,UIT}, param::LIFParameter{FT}, dt::FT, Ie::Vector{FT}) where {FT,UIT}
+    @unpack N, v, v_, spike, tlast, tcount = neurons
+    @unpack tref, tau_m, vrest, vreset, vthr, vpeak = param
+    
+    @inbounds for i = 1:N
+        vtmp = v[i]
+        tlasttmp = tlast[i]
+        
+        vtmp += dt * ((dt*tcount) > (tlasttmp + tref))*((vrest - vtmp + Ie[i]) / tau_m)
+        spiketmp = vtmp >= vthr
+
+        spike[i] = spiketmp
+        v_[i] = ifelse(spiketmp, vpeak, vtmp) #発火時の電位も含めて記録するための変数
+        v[i] = ifelse(spiketmp, vreset, vtmp)        
+        tlast[i] = ifelse(spiketmp, dt*tcount, tlasttmp) # 発火時刻の更新
+    end
+    # time count +1
+    neurons.tcount += 1
+end
+
 #= update関数の検証について
     N=1 [個], dt=0.01f0 [ms], T=450 [ms]の環境下で
     paramを引数として明示的に渡すと実行時間は約0.002
@@ -52,6 +72,28 @@ end
 
 # LIFNeuronに対するupdate!メソッドの定義
 function update_threads!(neurons::LIF{FT,UIT}, param::LIFParameter{FT}, dt::FT, Ie::SubArray) where {FT,UIT}
+    @unpack N, v, v_, spike, tlast, tcount = neurons
+    @unpack tref, tau_m, vrest, vreset, vthr, vpeak = param
+    
+    Threads.@threads for i = 1:N
+        @inbounds begin
+            vtmp = v[i]
+            tlasttmp = tlast[i]
+            
+            vtmp += dt * ((dt*tcount) > (tlasttmp + tref))*((vrest - vtmp + Ie[i]) / tau_m)
+            spiketmp = vtmp >= vthr
+    
+            spike[i] = spiketmp
+            v_[i] = ifelse(spiketmp, vpeak, vtmp) #発火時の電位も含めて記録するための変数
+            v[i] = ifelse(spiketmp, vreset, vtmp)        
+            tlast[i] = ifelse(spiketmp, dt*tcount, tlasttmp) # 発火時刻の更新
+        end
+    end
+    # time count +1
+    neurons.tcount += 1
+end
+
+function update_threads!(neurons::LIF{FT,UIT}, param::LIFParameter{FT}, dt::FT, Ie::Vector{FT}) where {FT,UIT}
     @unpack N, v, v_, spike, tlast, tcount = neurons
     @unpack tref, tau_m, vrest, vreset, vthr, vpeak = param
     
